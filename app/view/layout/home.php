5a757087d53d0c2a18c591e2af324df7c59ee151
vutienminh1919 <vutienminh1919@gmail.com>
2021-11-11 11:14:14 +0700

    add a file

--- app/view/layout/home.php
+++ app/view/layout/home.php
@@ -8,7 +8,7 @@ include_once "inc/carousel.php";
             <?php foreach ($products as $product): ?>
                 <div class="col-3 mb-3">
                     <div class="card ">
-                        <img src="" class="card-img-top" alt="...">
+                        <img src="<?php echo isset($product['image'])?$product['image']:'uploads/default.jpg';?>" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $product["name"] ?></h5>
                             <h6 class="card-text">
@@ -16,7 +16,7 @@ include_once "inc/carousel.php";
                             </h6>
                             <p class="card-text"><?php echo $product["description"] ?></p>
                             <button class="btn btn-primary">Mua</button>
-                            <a href="index.php?page=product-detail" class="btn btn-primary">Chi tiết sản phẩm</a>
+                            <a href="index.php?page=product-detail&id=<?php echo $product["id"] ?>" class="btn btn-primary">Chi tiết sản phẩm</a>
                         </div>
                     </div>
                 </div>
