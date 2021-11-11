be0ae6c7fa4c2445618b47eb8f82218aaa5a6b09
chithanhvtpt <chithanhvtpt@gmail.com>
2021-11-11 13:39:46 +0700

    css

--- app/view/layout/home.php
+++ app/view/layout/home.php
@@ -2,6 +2,23 @@
 include_once "inc/header.php";
 include_once "inc/nav.php";
 include_once "inc/carousel.php";
+include_once "app/controller/ProductController.php";
+include_once "app/controller/AuthController.php";
+include_once "app/controller/CategoryController.php";
+include_once "app/controller/UserController.php";
+include_once "app/model/CategoryModel.php";
+include_once "app/model/ProductModel.php";
+include_once "app/model/UserModel.php";
+$productController = new \App\controller\ProductController();
+$authController = new \App\controller\AuthController();
+$categoryController = new \App\controller\CategoryController();
+$userController = new \App\controller\UserController();
+$categoryModel = new \App\model\CategoryModel();
+$productModel = new \App\model\ProductModel();
+$userModel = new \App\model\UserModel();
+$page = (isset($_GET["page"])) ? $_GET["page"] : "";
+$username = ($_SESSION["username"] ?? "");
+$role = ($_SESSION["role"] ?? "");
 ?>
     <div class="container">
         <div class="row">
@@ -23,6 +40,16 @@ include_once "inc/carousel.php";
             <?php endforeach; ?>
         </div>
     </div>
+<?php
+switch ($page) {
+    case "product-list":
+        if ($role == 1) {
+            $authController->checkAuth();
+            $productController->index();
+        }
+        break;
+}
+?>
 <?php
 include_once "inc/foot.php";
 ?>
